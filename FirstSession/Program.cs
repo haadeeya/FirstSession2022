e7178fbb8d1b6471eaea6dc022db76bac948062c
Haadeeya <furreedunhaadeeya10@gmail.com>
2022-10-06 09:26:17 +0400

    removed commented lines

--- FirstSession/Program.cs
+++ FirstSession/Program.cs
@@ -21,62 +21,6 @@ namespace FirstSession
             category.Start();
             Console.ReadKey();
 
-            //Employee employee1 = new Employee();
-            //employee1.Name = "Jane Doe";
-
-            //Employee employee2 = employee1;
-            //employee2.Name = "James Doe";
-
-            //Employee employee3 = new Employee();
-            //employee3.Name = "James Doe";
-
-            //Console.WriteLine(employee1.Name);
-            //if(employee1.Name == employee2.Name)
-            //{
-            //    Console.WriteLine("True");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("False");
-            //}
-            //Console.WriteLine("Value: ");
-            //var value = int.Parse(Console.ReadLine());
-            //string msg = string.Empty;
-
-            //msg = value < 10 ? "Value less than 10" : value < 15 ? "Value between 10 and 15": "Value greater than 15";
-
-            //Console.WriteLine(msg);
-
-            //    var inputs = new List<int>() { 2, 3, 5, 6, 7, 9, 10 };
-
-            //    Console.WriteLine("Enter '1' for sum of even numbers only");
-            //    var choice = int.Parse(Console.ReadLine());
-
-            //    bool even = false;
-
-            //    even = choice == 1 ? true : false;
-
-            //    var total = Sum(inputs, even);
-
-            //    Console.WriteLine(total);
-
-            //    Console.ReadKey();
-            //}
-
-            //public static int Sum(List<int> input, bool even)
-            //{
-            //    var total = 0;
-            //    foreach (var i in input)
-            //    {
-            //        if(even)
-            //        {
-            //            total += i;
-            //        }
-
-            //    }
-            //    return total;
-            //}
-
 
         }
     }
