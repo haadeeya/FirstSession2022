90715ae33efcb175e27a66f0078ece14ef5139f3
Haadeeya <furreedunhaadeeya10@gmail.com>
2022-10-06 16:50:05 +0400

    removed comments

--- FirstSession/Program.cs
+++ FirstSession/Program.cs
@@ -1,13 +1,5 @@
 ﻿using ClassLibrary1;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.Remoting.Messaging;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirstSession
 {
